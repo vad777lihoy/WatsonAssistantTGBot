6c6b1b23a336da8282636d32aa0beecff6d04a0a
maximvolkov <maxix.5858@gmail.com>
2020-03-27 20:57:27 +0300

    added parse function

--- WA_bot.py
+++ WA_bot.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import ibm_watson
+import json
 from functools import wraps
 from dotenv import load_dotenv
 from telegram.ext import CommandHandler
@@ -10,6 +11,17 @@ from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.chataction import ChatAction
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_cloud_sdk_core.api_exception import ApiException
+
+# REQUEST_KWARGS = {
+#     'proxy_url': 'socks5://orbtl.s5.opennetwork.cc:999/',
+#     # Optional, if you need authentication:
+#     'urllib3_proxy_kwargs': {
+#         'assert_hostname': 'False',
+#         'cert_reqs': 'CERT_NONE',
+#         'username': '125145015',
+#         'password': 'xupI2fev'
+#     }
+# }
 
 load_dotenv()
 assistant_id = os.getenv('ASSISTANT_ID')
@@ -29,8 +41,43 @@ service.set_service_url(os.getenv('URL'))
 
 session_ids = {}
 
-updater = Updater(token=os.getenv('TOKEN'), use_context=True)
+updater = Updater(token=os.getenv('TOKEN'), use_context=True, request_kwargs=REQUEST_KWARGS)
 dispatcher = updater.dispatcher
+
+
+def parse_response(response):
+    reply_text = ''
+    labels = []
+    button_list = []
+    try:
+        for response_part in response['output']['generic']:
+            if response_part['response_type'] == 'text':
+                reply_text += response_part['text'] + '\n'
+            elif response_part['response_type'] == 'option':
+                reply_text += response_part['title']
+                labels = [option['label'] for option in response_part['options']]
+            elif response_part['response_type'] == 'suggestion':
+                reply_text += response_part['title']
+                labels = [suggestion['label'] for suggestion in response_part['suggestions']]
+                intents = [suggestion['value']['input']['intents'] for suggestion in response_part['suggestions']]
+                confs = [float(intent[0]['confidence']) for intent in intents if intent != []]
+                labels_confs = [(label, conf) for label, conf in zip(labels, confs)]
+                labels_confs = sorted(labels_confs, key=lambda x: x[1], reverse=True)
+                labels = [label_conf[0] for label_conf in labels_confs]
+            else:
+                reply_text += "Я вас не понял. Попробуйте пожалуйста перефразировать вопрос и я очень постараюсь вас " \
+                              "понять. "
+        button_list = [[s] for s in labels]
+    except IndexError:
+        reply_text = 'Watson Assistant is unavailable now :('
+    except KeyError:
+        reply_text = 'Ошибка обработки текста'
+
+    if len(button_list) > 0:
+        reply_markup = ReplyKeyboardMarkup(button_list)
+    else:
+        reply_markup = ReplyKeyboardRemove()
+    return reply_text, reply_markup
 
 
 def send_action(action):
@@ -60,25 +107,17 @@ def start(update, context):
         assistant_id,
         session_ids[user_id]
     ).get_result()
-    reply_text = ''
-    try:
-        reply_text = response['output']['generic'][0]['text']
-    except IndexError:
-        reply_text = 'Watson Assistant is unavailable now :('
-    reply_markup = ReplyKeyboardRemove()
+    with open('log.json', 'w') as f:
+        f.write(str(json.dumps(response, indent=4, ensure_ascii=False, )))
+    reply_text, reply_markup = parse_response(response)
     context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text, reply_markup=reply_markup)
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Помощь: /help')
+    # context.bot.send_message(chat_id=update.effective_chat.id, text='Помощь: /help')
 
 
 @send_action(ChatAction.TYPING)
 def help_user(update, context):
-    help_message = """
-    Команды бота:
-    Помощь: /help
-    Начало диалога: /start\n
-    
-    После отправки команды /start создается новая сессия с Watson Assistant.
-    """
+    help_message = "Команды бота:\nПомощь: /help\nНачало диалога: /start\nПосле отправки команды /start создается " \
+                   "новая сессия с ботом. "
     context.bot.send_message(chat_id=update.effective_chat.id, text=help_message)
 
 
@@ -91,44 +130,16 @@ def wa_reply(update, context):
         response = service.message(
             assistant_id,
             session_ids[user_id],
-            input={'text': update.message.text}
+            input={'text': update.message.text},
         ).get_result()
+        with open('log.json', 'w') as f:
+            f.write(str(json.dumps(response, indent=4, ensure_ascii=False, )))
     except ApiException:
         new_session(user_id)
         response = service.message(assistant_id, session_ids[user_id], input={'text': update.message.text}).get_result()
 
     logger.debug(response)
-    reply_text = ''
-    labels = []
-    button_list = []
-    try:
-        for response_part in response['output']['generic']:
-            if response_part['response_type'] == 'text':
-                reply_text += response_part['text'] + '\n'
-            elif response_part['response_type'] == 'option':
-                reply_text += response_part['title']
-                labels = [option['label'] for option in response_part['options']]
-            elif response_part['response_type'] == 'suggestion':
-                reply_text += response_part['title']
-                labels = [suggestion['label'] for suggestion in response_part['suggestions']]
-                intents = [suggestion['value']['input']['intents'] for suggestion in response_part['suggestions']]
-                confs = [float(intent[0]['confidence']) for intent in intents if intent != []]
-                labels_confs = [(label, conf) for label, conf in zip(labels, confs)]
-                labels_confs = sorted(labels_confs, key = lambda x: x[1], reverse=True) 
-                labels = [label_conf[0] for label_conf in labels_confs]
-            else:
-                reply_text += "Я вас не понял. Попробуйте пожалуйста перефразировать вопрос и я очень постараюсь вас понять."
-        button_list = [[s] for s in labels]
-    except IndexError:
-        reply_text = 'Watson Assistant is unavailable now :('
-    except KeyError:
-        reply_text = 'Ошибка обработки текста'
-
-    if len(button_list) > 0:
-        reply_markup = ReplyKeyboardMarkup(button_list)
-    else:
-        reply_markup = ReplyKeyboardRemove()
-    
+    reply_text, reply_markup = parse_response(response)
     context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text, reply_markup=reply_markup)
 
 
